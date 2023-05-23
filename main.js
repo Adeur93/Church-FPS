33741203030d0e3253b9240aace5731bd47eb20d
Jaime <jaimeruedagomezrgmail.com>
2023-05-23 15:24:38 -0500

    Inicié el menú principal

--- main.js
+++
@@ -1,7 +0,0 @@
-const { app, BrowserWindow } = require("electron");
-
-let mainWindow
-
-app.on("ready", () => {
-    mainWindow = BrowserWindow
-});
---
+++ src/main.js
@@ -0,0 +1,78 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const url = require("url");
+const path = require("path");
+
+
+if (process.env.NODE_ENV !== "production") {
+    require("electron-reload")(__dirname, {
+        electron: path.join(__dirname, "../node_modules", ".bin", "electron")
+    })
+}
+
+
+let mainWindow
+let VentanaAbout
+
+
+app.on("ready", () => {
+    mainWindow = new BrowserWindow({ width: 1280, height: 720 });
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, "views/index.html"),
+        protocol: "file",
+        slashes: true
+    }))
+    
+    const menusuperior = Menu.buildFromTemplate(menuprincipal);
+    Menu.setApplicationMenu(menusuperior);
+
+    mainWindow.on("closed", () => { app.quit(); })
+});
+
+
+function CrearVentanaAbout() {
+    VentanaAbout = new BrowserWindow({ width: 500, height: 500, });
+    VentanaAbout.setMenu(null);
+    VentanaAbout.loadURL(url.format({
+        pathname: path.join(__dirname, "views/about.html"),
+        protocol: "file",
+        slashes: true
+    }))
+}
+
+
+const menuprincipal = [
+    {
+        label: "Archivo",
+        submenu: [
+            { label: "Abrir guión", accelerator: "Ctrl+O" },
+            { label: "Guardar guión", accelerator: "Ctrl+S" },
+            { label: "Guardar guión como...", accelerator: "Ctrl+Shift+S" },
+            { type: "separator" },
+            { label: "Cerrar app", accelerator: "Ctrl+Q", click() { app.quit(); } }
+        ]
+    },
+    {
+        label: "Edición",
+        submenu: [
+            { label: "Deshacer", accelerator: "Ctrl+Z" },
+            { label: "Rehacer", accelerator: "Ctrl+Shift+Z" },
+            { type: "separator" },
+            { label: "Copiar", accelerator: "Ctrl+C" },
+            { label: "Cortar", accelerator: "Ctrl+X" },
+            { label: "Pegar", accelerator: "Ctrl+V" },
+        ]
+    },
+    {
+        label: "Importar",
+        submenu: [
+            { label: "Cancioneros" },
+            { label: "Versiones de la biblia" },
+            { label: "Imágenes" },
+            { label: "Videos" },
+        ]
+    },
+    {
+        label: "Acerca de la app",
+        click() { CrearVentanaAbout(); }
+    },
+];
