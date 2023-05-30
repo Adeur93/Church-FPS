faac7b84787f73c5bc9d16069dbf476c52738bc9
Jaime <jaimeruedagomezrgmail.com>
2023-05-30 16:40:05 -0500

    Aplicadas reglas ESlint al main

--- src/main.js
+++ src/main.js
@@ -1,153 +1,147 @@
-const { app, BrowserWindow, Menu, screen, } = require("electron");
-const url = require("url");
-const path = require("path");
+const { app, BrowserWindow, Menu, screen } = require('electron')
+const url = require('url')
+const path = require('path')
 
-if (process.env.NODE_ENV !== "production"){
-    require("electron-reload")(__dirname, {
-        electron: path.join(__dirname, "../node_modules", ".bin", "electron")
-    })
+if (process.env.NODE_ENV !== 'production') {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+  })
 }
 
-let MainWindow;
-let VentanaProyector;
+let MainWindow
+let VentanaProyector
 let AvisoPantala
 
+app.on('ready', () => {
+  const MenuSuperior = Menu.buildFromTemplate(MenuPrincipal)
+  Menu.setApplicationMenu(MenuSuperior)
 
-app.on("ready", () => {
+  MainWindow = new BrowserWindow({
+    show: false,
+    minHeight: 650,
+    minWidth: 800
+  })
+  MainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'views/index.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+  MainWindow.maximize()
+  MainWindow.show()
+  MainWindow.on('closed', () => { app.quit() })
 
-    const MenuSuperior = Menu.buildFromTemplate(MenuPrincipal);
-    Menu.setApplicationMenu(MenuSuperior);
+  const displays = screen.getAllDisplays()
+  const externalDisplay = displays.find((display) => {
+    return display.bounds.x !== 0 || display.bounds.y !== 0
+  })
 
-    MainWindow = new BrowserWindow({
-        show: false,
-        minHeight: 650,
-        minWidth: 800,
-    });
-    MainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, "views/index.html"),
-        protocol: "file",
-        slashes: true,
-    }));
-    MainWindow.maximize();
-    MainWindow.show();
-    MainWindow.on("closed", () => { app.quit(); });
-
-    const displays = screen.getAllDisplays()
-    const externalDisplay = displays.find((display) => {
-        return display.bounds.x !== 0 || display.bounds.y !== 0
+  if (externalDisplay) {
+    VentanaProyector = new BrowserWindow({
+      frame: false,
+      x: externalDisplay.bounds.x + 50,
+      y: externalDisplay.bounds.y + 50,
+      fullscreen: true
     })
+    VentanaProyector.setMenu(null)
+    VentanaProyector.loadURL(url.format({
+      pathname: path.join(__dirname, 'views/projection.html'),
+      protocol: 'file',
+      slashes: true
+    }))
+  }
 
-    if (externalDisplay) {
-        VentanaProyector = new BrowserWindow({
-            frame: false,
-            x: externalDisplay.bounds.x + 50,
-            y: externalDisplay.bounds.y + 50,
-            fullscreen: true,
-        });
-        VentanaProyector.setMenu(null)
-        VentanaProyector.loadURL(url.format({
-            pathname: path.join(__dirname, "views/projection.html"),
-            protocol: "file",
-            slashes: true
-        }));
-    }
+  if (!externalDisplay) {
+    AvisoPantala = new BrowserWindow({
+      width: 500,
+      height: 500,
+      resizable: false,
+      movable: false,
+      maximizable: false,
+      minimizable: false,
+      parent: MainWindow,
+      modal: true
+    })
+    AvisoPantala.loadURL(url.format({
+      pathname: path.join(__dirname, 'views/avisoNoPantalla2.html'),
+      protocol: 'file',
+      slashes: true
+    }))
+  }
+})
 
-    if (!externalDisplay) {
-        AvisoPantala = new BrowserWindow({
-            width: 500,
-            height: 500,
-            resizable: false,
-            movable: false,
-            maximizable: false,
-            minimizable: false,
-            parent: MainWindow,
-            modal: true
-        });
-        AvisoPantala.loadURL(url.format({
-            pathname: path.join(__dirname, "views/avisoNoPantalla2.html"),
-            protocol: "file",
-            slashes: true
-        }));    
-    }
-});
+app.on('window-all-closed', () => { app.quit() })
 
-
-app.on("window-all-closed", () => { app.quit() });
-
-
-function CrearVentanaAbout() {
-    const VentanaAbout = new BrowserWindow({
-        width: 500,
-        height: 500,
-        resizable: false,
-        movable: false,
-        maximizable: false,
-        minimizable: false,
-        parent: MainWindow,
-        modal: true
-    });
-    VentanaAbout.setMenu(null);
-    VentanaAbout.loadURL(url.format({
-        pathname: path.join(__dirname, "views/about.html"),
-        protocol: "file",
-        slashes: true
-    }));
+function CrearVentanaAbout () {
+  const VentanaAbout = new BrowserWindow({
+    width: 500,
+    height: 500,
+    resizable: false,
+    movable: false,
+    maximizable: false,
+    minimizable: false,
+    parent: MainWindow,
+    modal: true
+  })
+  VentanaAbout.setMenu(null)
+  VentanaAbout.loadURL(url.format({
+    pathname: path.join(__dirname, 'views/about.html'),
+    protocol: 'file',
+    slashes: true
+  }))
 };
 
-
-function CrearVentanaAjustes() {
-    const VentanaAjustes = new BrowserWindow({
-        width: 900,
-        height: 900,
-        maximizable: false,
-        minimizable: false,
-        resizable: false,
-        parent: MainWindow,
-        modal: true
-    });
-    VentanaAjustes.setMenu(null);
-    VentanaAjustes.loadURL(url.format({
-        pathname: path.join(__dirname, "views/ajustes.html"),
-        protocol: "file",
-        slashes: true
-    }));
+function CrearVentanaAjustes () {
+  const VentanaAjustes = new BrowserWindow({
+    width: 900,
+    height: 900,
+    maximizable: false,
+    minimizable: false,
+    resizable: false,
+    parent: MainWindow,
+    modal: true
+  })
+  VentanaAjustes.setMenu(null)
+  VentanaAjustes.loadURL(url.format({
+    pathname: path.join(__dirname, 'views/ajustes.html'),
+    protocol: 'file',
+    slashes: true
+  }))
 };
-
 
 const MenuPrincipal = [
-    {
-        label: "Archivo",
-        submenu: [
-            { label: "Abrir guión", accelerator: "CommandOrControl+O" },
-            { label: "Guardar guión", accelerator: "CommandOrControl+S" },
-            { label: "Guardar guión como...", accelerator: "CommandOrControl+Shift+S" },
-            { type: "separator" },
-            { label: "Cerrar app", accelerator: "CommandOrControl+Q", click() { app.quit(); } }
-        ]
-    },
-    {
-        label: "Edición",
-        submenu: [
-            { label: "Deshacer", accelerator: "CommandOrControl+Z" },
-            { label: "Rehacer", accelerator: "CommandOrControl+Shift+Z" },
-            { type: "separator" },
-            { label: "Copiar", accelerator: "CommandOrControl+C" },
-            { label: "Cortar", accelerator: "CommandOrControl+X" },
-            { label: "Pegar", accelerator: "CommandOrControl+V" },
-            { type: "separator" },
-            { label: "Ajustes", click() { CrearVentanaAjustes(); } }
-        ]
-    },
-    {
-        label: "Importar",
-        submenu: [
-            { label: "Imágenes" },
-            { label: "Videos" },
-            { label: "presentaciones"}
-        ]
-    },
-    {
-        label: "Acerca de la app",
-        click() { CrearVentanaAbout(); }
-    },
-];
+  {
+    label: 'Archivo',
+    submenu: [
+      { label: 'Abrir guión', accelerator: 'CommandOrControl+O' },
+      { label: 'Guardar guión', accelerator: 'CommandOrControl+S' },
+      { label: 'Guardar guión como...', accelerator: 'CommandOrControl+Shift+S' },
+      { type: 'separator' },
+      { label: 'Cerrar app', accelerator: 'CommandOrControl+Q', click () { app.quit() } }
+    ]
+  },
+  {
+    label: 'Edición',
+    submenu: [
+      { label: 'Deshacer', accelerator: 'CommandOrControl+Z' },
+      { label: 'Rehacer', accelerator: 'CommandOrControl+Shift+Z' },
+      { type: 'separator' },
+      { label: 'Copiar', accelerator: 'CommandOrControl+C' },
+      { label: 'Cortar', accelerator: 'CommandOrControl+X' },
+      { label: 'Pegar', accelerator: 'CommandOrControl+V' },
+      { type: 'separator' },
+      { label: 'Ajustes', click () { CrearVentanaAjustes() } }
+    ]
+  },
+  {
+    label: 'Importar',
+    submenu: [
+      { label: 'Imágenes' },
+      { label: 'Videos' },
+      { label: 'presentaciones' }
+    ]
+  },
+  {
+    label: 'Acerca de la app',
+    click () { CrearVentanaAbout() }
+  }
+]
